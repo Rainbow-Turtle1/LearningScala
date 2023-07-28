461bc857e268968df020f1206de2deb6ee173783
Rainbow-Turtle1 <72671611+Rainbow-Turtle1@users.noreply.github.com>
2023-07-28 12:55:08 +0100

    initial commit

---
+++ src/Playground/ScalaPlayground.scala
@@ -0,0 +1,17 @@
+package Playground
+
+object ScalaPlayground extends App { // extending the existing app
+  println("Hello scala")
+
+}
+
+// ALTERNATIVE METHOD
+
+//package Playground
+//
+//object ScalaPlayground {
+//  def main(args: Array[String]): Unit = { // creating a main function that runs
+//    println("Hello scala")
+//
+//  }
+//}
